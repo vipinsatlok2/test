eae3aa6542a1cb0ffce9a089c25e0bc5835a548b
vipinsatlok2 <127262798+vipinsatlok2@users.noreply.github.com>
2023-03-13 20:23:48 +0000

    fs 2

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
+require("dotenv").config()
 const fs = require('@cyclic.sh/s3fs')
 
 fs.writeFile("satsaheb.txt", "cyclic server me fs vala kaam kar raha hai", "utf-8", (err) => {
     if (err) return console.log("1", err)
     console.log("1")
 
-    fs.readFile("satsaheb.txt", "utf-8", (err, data) => {
+    fs.readFile("satsaheb.txt", (err, data) => {
         if (err) return console.log("2", err)
         console.log("2", data)
 
