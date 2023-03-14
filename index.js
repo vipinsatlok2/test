96940d6b900f78946fcf393f3d3352c993859b7e
vipinsatlok2 <127262798+vipinsatlok2@users.noreply.github.com>
2023-03-14 13:50:40 +0000

    save

--- index.js
+++ index.js
@@ -1,14 +1,51 @@
-require("dotenv").config()
-const fs = require('@cyclic.sh/s3fs')
+const { google } = require("googleapis");
+const fs = require("@cyclic.sh/s3fs");
+const stream = require("stream");
 
-fs.writeFile("satsaheb.txt", "cyclic server me fs vala kaam kar raha hai", "utf-8", (err) => {
-    if (err) return console.log("1", err)
-    console.log("1")
+// Authenticate your app
+const auth = new google.auth.GoogleAuth({
+  keyFile: "secret.json",
+  scopes: ["https://www.googleapis.com/auth/drive.file"],
+});
 
-    fs.readFile("satsaheb.txt",  (err, data) => {
-        if (err) return console.log("2", err)
-        console.log("2", data.toString('utf-8'))
+// Create a client for Google Drive API
+const drive = google.drive({ version: "v3", auth });
 
-        fs.unlink("satsaheb.txt", (err) => console.log("3", err, "deleted"))
-    })
-})
+// Read the image file
+const imageFile = "image.jpg";
+
+fs.readFile(imageFile, (err, data) => {
+  if (err) {
+    console.error("Error reading file:", err);
+    return;
+  }
+
+  // ID of the folder where you want to upload the image file
+  const folderId = "1SkAK09omCBOFRvK0GRXXOjE5_75FisF2";
+
+  // // Create a new file in Google Drive
+  const readable = new stream.PassThrough();
+  readable.end(data);
+
+  drive.files.create(
+    {
+      requestBody: {
+        name: "satsaheb.jpg",
+        mimeType: "image/jpg",
+        parents: [folderId], // Set the ID of the folder where you want to upload the image file
+      },
+      media: {
+        mimeType: "image/jpg",
+        body: readable,
+      },
+    },
+    (err, file) => {
+      if (err) {
+        console.error("Error uploading file:", err);
+        return;
+      }
+
+      console.log("File uploaded:", file.data.webViewLink);
+    }
+  );
+});
